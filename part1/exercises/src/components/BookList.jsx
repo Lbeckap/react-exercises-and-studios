769e7a98ef55f7ece6cf1a82ce2385624c50c649
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-08-19 02:58:07 -0400

    react1 exercise

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1575009552i/18693655.jpg";
+   let book2 = "https://cms-imgp.jw-cdn.org/img/p/1001070103/univ/art/1001070103_univ_sqr_lg.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/91s6WKPM09L._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="A Mind for Numbers: How to Excel at Math and Science (Even If You Flunked Algebra)" width={"30%"}/>
+         <img src={book2} alt="New World Translation of the Holy Scriptures (Study Edition)" width={"30%"}/>
+         <img src={book3} alt="Ray Bradbury's Fahrenheit 451" width={"30%"}/>
       </div>      
    );
 }
