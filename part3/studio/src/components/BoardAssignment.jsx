d2b9c88b1c316b31610295c469c54506439c4bc3
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-08-30 11:52:34 -0400

    react 3 studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [{
+      label: 'dessert',
+      value: 'dessert',
+   }, 
+   {
+      label: 'baking',
+      value: 'baking',
+   }, 
+   {
+      label: 'peaches',
+      value: 'peaches',
+   }];
+
+   const [boardName, setName] = useState('no boards yet!');
 
    const handleChange = (event) => {
+      setName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {console.log(boards.length)}
+         {boards.map((board, index) => 
+            <option value={board.value} key={index}>{board.value}</option>
+         )}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,65 +1,79 @@
 import { useState } from 'react';
 
-const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
 
-   return (
+const RecipeAuthor = () => {
+  const authorLink =
+    'https://www.crazyforcrust.com/peach-pie-double-crust-or-crumble-recipe/';
+  const authorPhoto =
+    'https://secure.gravatar.com/avatar/ee59140562b910249161e8139c3ab3a2?s=222&r=g';
+  const authorName = 'Dorothy Kern';
+
+  return (
+    <div>
+      <img
+        src={authorPhoto}
+        alt={authorName}
+        style={{ objectFit: 'contain', borderRadius: '50%' }}
+      />
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
-         <div>
-            <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
-         </div>
+        <h3>{authorName}</h3>
+        <a href={authorLink}></a>
       </div>
-   );
-}
+    </div>
+  );
+};
 
 const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
-      <div>
-         <h3>Recipe Ingredients</h3>
-         <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
-         </ul>
-      </div>
-   );
-}
+  const ingredients = [
+    'All butter pie crust',
+    '5 cups fresh or thawed frozen peaches (about 2 pounds)',
+    '⅔ cup (134g) granulated sugar',
+    '½ teaspoon ground cinnamon',
+    '1 tablespoon lemon juice',
+  ];
+  return (
+    <div>
+      <h3>Recipe Ingredients</h3>
+      <ul>
+        <li>{ingredients[0]}</li>
+        <li>{ingredients[1]}</li>
+        <li>{ingredients[2]}</li>
+        <li>{ingredients[3]}</li>
+        <li>{ingredients[4]}</li>
+      </ul>
+    </div>
+  );
+};
 
 const RecipeDescription = () => {
-   return (
-      <div> 
-         <div>
-            <h1></h1>
-            <p></p>
-         </div>
-         <div className="recipePhotoBlock">
-            <RecipeIngredients />
-            <RecipeAuthor />
-         </div>
+  return (
+    <div>
+      <div>
+        <h1>Peach Pie Recipe</h1>
+        <p>
+          A peach pie can be made with a double crust, lattice crust, or even a
+          crumble topping. This peach pie recipe is easy and fast and perfect!
+        </p>
       </div>
-   );
-}
+      <div className='recipePhotoBlock'>
+        <RecipeIngredients />
+        <RecipeAuthor />
+      </div>
+    </div>
+  );
+};
 
 const RecipePhoto = () => {
-   return (
-      <img src="" alt="" className="imageUpdates"/>
-   );
-}
+  return <img src="https://www.crazyforcrust.com/wp-content/uploads/2024/05/peach-pie-6-368x368.jpg" alt='Peach pie' className='imageUpdates' />;
+};
 
-export default function RecipeDisplay () {
-   return(
-      <div className="recipePhotoBlock">
-         <RecipePhoto />
-         <div>
-            <RecipeDescription />
-         </div>
+export default function RecipeDisplay() {
+  return (
+    <div className='recipePhotoBlock'>
+      <RecipePhoto />
+      <div>
+        <RecipeDescription />
       </div>
-   );
+    </div>
+  );
 }
